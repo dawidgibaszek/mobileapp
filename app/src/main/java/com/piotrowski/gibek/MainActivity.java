90e1cd18596546b929e1fe15745af6cc12d4ec0e
ukiyo <36029677+ukiyo-art@users.noreply.github.com>
2024-02-19 22:33:03 +0100

    zadanie 19.02

--- app/src/main/java/com/piotrowski/gibek/MainActivity.java
+++ app/src/main/java/com/piotrowski/gibek/MainActivity.java
@@ -1,39 +1,45 @@
 package com.piotrowski.gibek;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int clickCount = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        TextView txt1 = findViewById(R.id.txt1);
+        TextView txt2 = findViewById(R.id.txt2);
+        TextView txt3 = findViewById(R.id.txt3);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                clickCount++;
-                if(clickCount > 2) clickCount = 0;
-                switch(clickCount) {
-                    case 1:
-                        btn.setText("Dzień dobry");
-                        break;
-                    case 2:
-                        btn.setText("Do widzenia");
-                        break;
-                    case 0:
-                        btn.setText("Kliknij");
-                    default:
-                        btn.setText("Kliknij");
-                }
+                txt1.setVisibility(View.VISIBLE);
+            }
+        });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                txt2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                txt3.setVisibility(View.VISIBLE);
             }
         });
     }
